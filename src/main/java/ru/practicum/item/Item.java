64b0e2be626092cfaac1c62d728c874a478ecaa0
ElmoCrumley <MW826124@gmail.com>
2026-06-15 15:00:31 +0300

    Создать UserState. Добавить аннотации @Table, @Id, @Column, @Enumerated в классы Item, User.

--- src/main/java/ru/practicum/item/Item.java
+++ src/main/java/ru/practicum/item/Item.java
@@ -1,10 +1,23 @@
 package ru.practicum.item;
 
-import lombok.Data;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter @Setter
+@Entity
+@Table(name = "items", schema = "public")
 class Item {
+
+    @Id
     private Long id;
+
+    @Column(name = "userId", nullable = false)
     private Long userId;
+
+    @Column(name = "url")
     private String url;
 }
--- src/main/java/ru/practicum/user/User.java
+++ src/main/java/ru/practicum/user/User.java
@@ -1,10 +1,30 @@
 package ru.practicum.user;
 
-import lombok.Data;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+import java.time.Instant;
+
+@Getter @Setter
+@Entity
+@Table(name = "users", schema = "public")
 public class User {
+
+    @Id
     private Long id;
+
+    @Column(name = "firstName", nullable = false)
+    private String firstName;
+
+    @Column(name = "lastName")
+    private String lastName;
+
     private String email;
-    private String name;
+
+    @Column(name = "registration_date")
+    private Instant registrationDate = Instant.now();
+
+    @Enumerated(EnumType.STRING)
+    private UserState state;
 }
