d1b649e3ca4558f62bada741580e9b2969441d57
ElmoCrumley <MW826124@gmail.com>
2026-06-18 14:02:59 +0300

    Реализовать транзакции.

--- src/main/java/ru/practicum/item/ItemServiceImpl.java
+++ src/main/java/ru/practicum/item/ItemServiceImpl.java
@@ -2,11 +2,13 @@ package ru.practicum.item;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class ItemServiceImpl implements ItemService {
     private final ItemRepository repository;
 
@@ -16,12 +18,14 @@ public class ItemServiceImpl implements ItemService {
     }
 
     @Override
+    @Transactional
     public ItemDTO addNewItem(long userId, Item item) {
         item.setUserId(userId);
         return ItemDTO.from(repository.save(item));
     }
 
     @Override
+    @Transactional
     public void deleteItem(long userId, long itemId) {
         repository.deleteByUserIdAndItemId(userId, itemId);
     }
